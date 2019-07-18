2c1994f1a16193ffec0384220078d1f05dfedb63
MurungaKibaara <murungakibaara@outlook.com>
2019-07-18 19:45:25 +0300

    [Created houses model]

--- app/api/v1/models/house_models.py
+++ app/api/v1/models/house_models.py
@@ -1,4 +1,4 @@
-'''Properties model: Class to manage property data'''
+'''Properties model: Class to manage house data'''
 import datetime
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -11,14 +11,14 @@ JWT_SECRET = Config.SECRET_KEY
 
 INIT_DB = init_db()
 
-class PropertyRecords():
+class HouseRecords():
     """ Create a model that stores property data"""
 
     def __init__(self):
         """initialize the database and argument variables"""
         self.database = INIT_DB
 
-    def register_property(self, property_name):
+    def register_house(self, house_number, house_type, rent_amount):
         """ Add a new property """
 
         auth_header = request.headers.get('Authorization')
@@ -27,62 +27,64 @@ class PropertyRecords():
         decoded_token = jwt.decode(auth_token, JWT_SECRET, algorithms='HS256')
         landlord_id = decoded_token['sub']
 
+        property_id_query = ('SELECT property_id FROM property WHERE landlord_id='%(landlord_id)s')
+
         payload = {
-            "property_name": property_name,
-            "landlord_id": landlord_id
+            "property_id": property_id,
+            "house_number": house_number,
+            "house_type": house_type,
+            "rent_amount": rent_amount
         }
 
-
-        query = """INSERT INTO property (property_name, landlord_id) VALUES (%(property_name)s, %(landlord_id)s);"""
-
+        query = """INSERT INTO houses (rent_amount, house_type, house_number, property_id) VALUES (%(rent_amount)s,%(house_type)s,%(house_number)s, %(property_id)s);"""
 
         cur = self.database.cursor()
         cur.execute(query, payload)
         self.database.commit()
 
-        return jsonify({"message": ("property %s successfully registered")%(property_name)}), 201
+        return jsonify({"message": ("house %s %s successfully registered")%(house_type, house_number)}), 201
 
-    def view_properties(self):
-        '''View all properties'''
+    def view_houses(self):
+        '''View all houses '''
         try:
             cur = self.database.cursor(cursor_factory=RealDictCursor)
-            cur.execute("""  SELECT * FROM property """)
+            cur.execute("""  SELECT * FROM houses """)
             data = cur.fetchall()
 
             if data == None:
-                return jsonify({"message":"no properties found"})
+                return jsonify({"message":"no houses found"})
 
-            return jsonify({"properties": data})
+            return jsonify({"houses": data})
 
         except (psycopg2.Error) as error:
             return jsonify(error)
 
-    def view_property(self, property_id):
-        '''View a particular property'''
+    def view_house(self, house_id):
+        ''' View a particular house '''
         try:
             cur = self.database.cursor(cursor_factory=RealDictCursor)
-            cur.execute("""  SELECT * FROM property WHERE property_id = '%s' """ % (property_id))
+            cur.execute("""  SELECT * FROM houses WHERE house_id = '%s' """ % (house_id))
             data = cur.fetchone()
 
-            if len(data) is None:
+            if data == None:
                 return jsonify({"message":"property does not exist"})
 
-            return jsonify({"property ": data})
+            return jsonify({"houses ": data})
 
         except (psycopg2.Error) as error:
             return jsonify(error)
     
-    def view_property_by_name(self, property_name):
+    def view_house_by_number(self, house_number):
         '''View a particular property by name(To allow for search)'''
         try:
             cur = self.database.cursor(cursor_factory=RealDictCursor)
-            cur.execute("""  SELECT * FROM property WHERE property_name = '%s' """ % (property_name))
+            cur.execute("""  SELECT * FROM houses WHERE house_number = '%s' """ % (house_number))
             data = cur.fetchone()
 
             if data == None:
-                return jsonify({"message":"property does not exist"})
+                return jsonify({"message":"house does not exist"})
 
-            return jsonify({"property ": data})
+            return jsonify({"houses ": data})
 
         except (psycopg2.Error) as error:
             return jsonify(error)
